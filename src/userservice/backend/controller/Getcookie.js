37bb54fe399383a3478b9d0b6b5f462f52b80597
Hariraghav.S <hariraghava21s@gmail.com>
2025-08-14 01:15:58 +0530

    Cors policy-13-08-2025

--- src/userservice/backend/controller/Getcookie.js
+++ src/userservice/backend/controller/Getcookie.js
@@ -1,6 +1,6 @@
 exports.getCookie = async (req, res) => {
   const cookieName = req.body.name; // name sent from frontend
   const cookieValue = req.cookies[cookieName]; // dynamic access
-  if (!cookieValue) return res.status(404).json({ error: "Cookie not found" });
+  if (!cookieValue) return res.status(204).json({ error: "Cookie not found" });
   return res.status(200).json({ cookie: cookieValue });
 };
